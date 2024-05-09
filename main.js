9372cee868db0a81729733016cf36adf3c967f4a
zwaxi <muhammadishkuvatov@gmail.com>
2024-05-09 14:06:37 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function hello(a,b) {
+    alert("Kiritilgan sonlarning eng kotta soni:  " + Math.max(a,b))
+   
+   }
+   
+   hello(prompt("Ihtiyoriy son kiriting..."),prompt("Ihtiyoriy son kiriting"));
+   
+   
